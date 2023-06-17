28eba4b0745e2830a450b756df9829e5506f643c
Jesther Costinar <jesther.jc15@gmail.com>
2023-06-18 03:46:07 +0800

    Controller to registration page

--- src/main/java/com/jesthercostinar/springboot/controller/AuthController.java
+++ src/main/java/com/jesthercostinar/springboot/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.jesthercostinar.springboot.controller;
 
+import com.jesthercostinar.springboot.dto.UserDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +11,14 @@ public class AuthController {
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        // Create model object to store form data
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+
+        return "register";
     }
 }
