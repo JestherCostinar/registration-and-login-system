0027824cf386f29855ab26358930097546d6bf36
Jesther Costinar <jesther.jc15@gmail.com>
2023-06-19 00:24:59 +0800

    List of users backend and frontend

--- src/main/java/com/jesthercostinar/springboot/service/UserService.java
+++ src/main/java/com/jesthercostinar/springboot/service/UserService.java
@@ -3,8 +3,12 @@ package com.jesthercostinar.springboot.service;
 import com.jesthercostinar.springboot.dto.UserDto;
 import com.jesthercostinar.springboot.entity.User;
 
+import java.util.List;
+
 public interface UserService {
     void saveUser(UserDto userDto);
 
     User findUserByEmail(String email);
+
+    List<UserDto> findAllUsers();
 }
